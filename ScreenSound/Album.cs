b5d364307904d3c308bbd21283436ac932ec5b17
Douglas Garcia Rangel <douglasgovmad@gmail.com>
2026-04-07 00:27:27 -0300

    adicionada classe album

---
+++ ScreenSound/Album.cs
@@ -0,0 +1,15 @@
+﻿class Album
+{
+    private List<Musica> musicas = new List<Musica>();
+    public string Nome { get; set; }
+    public int DuracaoTotal { get; set; }
+    public void AdicionarMusica(Musica musica)
+    {
+        if (musicas == null)
+        {
+            musicas.Add(musica);
+        }
+
+        Console.WriteLine($"Música '{musica.nome}' adicionada ao álbum '{Nome}'.");
+    }
+}
--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,14 +1,17 @@
-﻿Musica musica1 = new Musica();
-musica1.nome = "Roxane";
-musica1.artista = "The Police";
-musica1.duracao = 273;
+﻿Album albumDoMisfits = new Album();
+albumDoMisfits.Nome = "Famous Monsters";
+
+Musica musica1 = new Musica();
+musica1.nome = "Scream";
+musica1.artista = "Misfits";
+musica1.duracao = 180;
 musica1.disponivel = true;
 
 Musica musica2 = new Musica();
-musica2.nome = "Vertigo";
-musica2.artista = "U2";
-musica2.duracao = 367;
+musica2.nome = "Astro Zombies";
+musica2.artista = "Misfits";
+musica2.duracao = 210;
 musica2.disponivel = false;
 
-musica1.ExibirFichaTecnica();
-musica2.ExibirFichaTecnica();
+albumDoMisfits.AdicionarMusica(musica1);
+albumDoMisfits.AdicionarMusica(musica2);
